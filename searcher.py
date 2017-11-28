8c52cea983d5339c3e3bb7d141000675ae2adfd7
Jon Rolfe <jon-rolfe@users.noreply.github.com>
2017-11-28 01:01:03 -0500

    Initial commit

---
+++ searcher.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+"""
+Usage: ./goodanalyzer.py database
+"""
+
+import os
+import sys
+import argparse
+import signal
+import logging
+from lxml import html
+import requests
+from selenium import webdriver
+
+
+def main():
+    """Does most of the magic."""
+
+    geckopath = '/Users/jonathanrolfe/Desktop/geckodriver'
+
+    browser = webdriver.Firefox(executable_path=geckopath)
+
+    browser.get('https://www.goodreads.com/search?q=science+fiction&search_type=books&search%5Bfield%5D=genre')
+
+    book_title_location = '/html/body/div[1]/div[2]/div[1]/div[1]/div[2]/table/tbody/tr[{}]/td[2]/a'
+    page = 1
+
+    while page < 101:
+        book_on_page = 1
+        while book_on_page < 21:
+            print browser.find_element_by_xpath(book_title_location.format(book_on_page)).get_attribute('href')
+            book_on_page += 1
+
+        browser.find_element_by_class_name('next_page').click()
+        page += 1
+
+
+def close(*args):
+    """Handles sigint/unexpected program exit"""
+    sys.exit(1)
+
+
+if __name__ == "__main__":
+    # make a SIGINT handler for ctrl-c, etc
+    signal.signal(signal.SIGINT, close)
+    # call main
+    main()
