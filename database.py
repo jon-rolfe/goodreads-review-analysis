d987f05b4517423f1b1ee261273c43b4cfb4f562
Jon Rolfe <jon-rolfe@users.noreply.github.com>
2017-11-28 03:13:10 -0500

    Added parsing logic to the search through the wonders of XPath.

--- database.py
+++ database.py
@@ -7,27 +7,38 @@ import sqlite3
 
 # initialize the things and stuff. TODO: prettify
 DATABASE_LOCATION = 'gr-sf.sqlite'
-DATABASE = sqlite3.connect(DATABASE_LOCATION)
-DATABASE_CURSOR = DATABASE.cursor()
+BOOK_DB = sqlite3.connect(DATABASE_LOCATION)
+DATABASE_CURSOR = BOOK_DB.cursor()
 
 
 def init_database():
     """Creates a SQLite DB if one doesn't already exist."""
 
     # Check DB integrity
-    DATABASE.execute('PRAGMA quick_check')
-    if DATABASE.fetchone()[0] != 'ok':
+    BOOK_DB.execute('PRAGMA quick_check')
+    if BOOK_DB.fetchone()[0] != 'ok':
         print 'Due to a problem, the database must be rebuilt.'
         destroy_database()
+
+    BOOK_DB.execute('''
+      CREATE TABLE IF NOT EXISTS `scifi` (
+        `id` INTEGER PRIMARY KEY,
+        `title` TEXT,
+        `author` TEXT,
+        `avg_rating` REAL,
+        `num_ratings` INTEGER,
+        `publish_year` INTEGER
+      )
+    ''')
 
 
 def destroy_database():
     """Drops the DB tables and makes new ones."""
 
     # Drop tables, commit, make a new one.
-    DATABASE.execute('''
-        DROP TABLE IF EXISTS goodreads
+    BOOK_DB.execute('''
+        DROP TABLE IF EXISTS `scifi`
     ''')
-    DATABASE.commit()
+    BOOK_DB.commit()
 
     init_database()
--- searcher.py
+++ searcher.py
@@ -10,7 +10,7 @@ import sys
 from selenium import webdriver
 
 
-# import logging (TODO)
+# import logging - TODO: real logging
 
 
 def main():
@@ -20,15 +20,29 @@ def main():
 
     browser = webdriver.Firefox(executable_path=geckopath)
 
-    browser.get('https://www.goodreads.com/search?q=science+fiction&search_type=books&search%5Bfield%5D=genre')
+    search_term = 'science+fiction'
+    browser.get('https://www.goodreads.com/search?q={}&search_type=books&search%5Bfield%5D=genre'.format(search_term))
 
-    book_title_location = '/html/body/div[1]/div[2]/div[1]/div[1]/div[2]/table/tbody/tr[{}]/td[2]/a'
+    book_root_location = '/html/body/div[1]/div[2]/div[1]/div[1]/div[2]/table/tbody/tr[{}]/'
+    book_title_location = book_root_location + 'td[1]/a'
+    book_url_location = book_root_location + 'td[1]/a'
+    book_author_location = book_root_location + 'td[2]/span[2]/a'
+    book_rating_info_location = book_root_location + 'td[2]/span[3]/span'
+    book_publish_year = book_root_location + 'td[2]/span[3]'
+
     page = 1
 
-    while page < 101:
+    while page < 101:  # GR has max 100 pages per search term
         book_on_page = 1
         while book_on_page < 21:
-            print browser.find_element_by_xpath(book_title_location.format(book_on_page)).get_attribute('href')
+            print 'title: ' + browser.find_element_by_xpath(book_title_location.format(book_on_page)).get_attribute(
+                'title')
+            print 'url: ' + browser.find_element_by_xpath(book_url_location.format(book_on_page)).get_attribute('href')
+            print 'author: ' + browser.find_element_by_xpath(book_author_location.format(book_on_page)).text
+            print 'rating info: ' + browser.find_element_by_xpath(book_rating_info_location.format(book_on_page)).text
+            print 'publish year: ' + \
+                  browser.find_element_by_xpath(book_publish_year.format(book_on_page)).text.split('published ', 1)[1][
+                  :4] + '\n'  # TODO: oh god please make this less hacky
             book_on_page += 1
 
         browser.find_element_by_class_name('next_page').click()
