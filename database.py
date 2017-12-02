b8283d35f2b29768a039f2a1703d9152c1c5afa2
Jon Rolfe <jon-rolfe@users.noreply.github.com>
2017-12-02 16:57:39 -0500

    Added: Table for reviews, book caller function, book list size function, basic logging

--- database.py
+++ database.py
@@ -2,8 +2,11 @@
 Database logic for all things Goodreads.
 """
 
+import logging
 # import datetime
 import sqlite3
+
+logger = logging.getLogger('__name__')
 
 # initialize the things and stuff. TODO: prettify
 BOOK_DB_LOCATION = 'goodreads-scifi.sqlite'
@@ -18,13 +21,13 @@ def init_database():
     # Check DB integrity
     BOOK_DB_CURSOR.execute('PRAGMA QUICK_CHECK')
     if BOOK_DB_CURSOR.fetchone()[0] != 'ok':
-        print 'Due to a problem, the database must be rebuilt.'
+        logger.error('Due to a problem, the database must be rebuilt.')
         remake_database()
 
     BOOK_DB_CURSOR.execute('''
       CREATE TABLE IF NOT EXISTS "book_list" (
         "id" INTEGER PRIMARY KEY,
-        "url" TEXT,
+        "url" TEXT UNIQUE,
         "title" TEXT,
         "author" TEXT,
         "publish_year" INTEGER,
@@ -33,11 +36,17 @@ def init_database():
     ''')
 
     BOOK_DB_CURSOR.execute('''
-      CREATE TABLE IF NOT EXISTS "reviews"
+      CREATE TABLE IF NOT EXISTS "reviews" (
         "id" INTEGER PRIMARY KEY,
-        
+        "book_id" INTEGER,
+        "stars" INTEGER,
+        "post_date" TEXT,
+        "url" TEXT UNIQUE,
+        FOREIGN KEY("book_id") REFERENCES book_list("id")
+      )
     ''')
 
+    logger.debug('CREATE TABLE IF NOT EXIST statements successful')
     BOOK_DB.commit()
 
 
@@ -61,4 +70,27 @@ def add_book(title, author, publish_year, url):
         VALUES (?, ?, ?, ?)
     ''', (title, author, publish_year, url))
 
+    # TODO: consider moving commit statement outside function - inefficient to commit every time when bulk adding
     BOOK_DB.commit()
+
+
+def call_book(id):  # TODO: split this into two fns
+    """Grab a book from the table by its ID"""
+    BOOK_DB_CURSOR.execute('''
+        SELECT * FROM "book_list" WHERE "ID" = ?
+    ''', (id))
+    result = BOOK_DB_CURSOR.fetchone()
+
+    if result is None:  # i.e., there is no book with a given ID
+        return False
+    else:
+        return result
+
+
+def book_list_size():
+    """Get the number of books in the book_list table (for reference)"""
+    BOOK_DB_CURSOR.execute('''
+        SELECT COUNT(*) FROM "book_list"
+    ''')
+
+    return BOOK_DB_CURSOR.fetchone()[0]
